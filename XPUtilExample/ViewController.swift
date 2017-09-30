075ded6f6f9be282f78e0ee1c95c22affd6624ee
lixp <420436097@qq.com>
2017-09-30 10:15:38 +0800

    添加Swift工具类

---
+++ XPUtilExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XPUtilExample
+//
+//  Created by xyj on 2017/9/30.
+//  Copyright © 2017年 xyj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
