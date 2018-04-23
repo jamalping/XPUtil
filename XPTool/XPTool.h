2db5862f83408a48b256ca2f3c9ee18204d80155
jamalping <420436097@qq.com>
2018-04-23 14:53:42 +0800

    增加打包framework的target

---
+++ XPTool/XPTool.h
@@ -0,0 +1,19 @@
+//
+//  XPTool.h
+//  XPTool
+//
+//  Created by xp on 2018/4/23.
+//  Copyright © 2018年 xyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XPTool.
+FOUNDATION_EXPORT double XPToolVersionNumber;
+
+//! Project version string for XPTool.
+FOUNDATION_EXPORT const unsigned char XPToolVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XPTool/PublicHeader.h>
+
+
